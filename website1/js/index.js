58b2676bcfd432b76d070fa77d80758a6e8fefa7
Garrett Katashima <gkatashima@gmail.com>
2017-08-12 17:50:13 -0700

    update files

--- website1/js/index.js
+++ website1/js/index.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
   var $documentBody = $('body'),
-    $submissionModal = $('.submission-modal'),
+    $submissionModal = $('.demo-submission-modal'),
     CLASS_NAMES = {
       OPEN: 'open',
-      REQUEST_BUTTON: 'btn-info',
-      SUBMISSION_MODAL: 'submission-modal',
-      SUBMIT_FORM_BUTTON: 'btn-default'
+      BTN_INFO: 'btn-info',
+      SUBMISSION_MODAL: 'demo-submission-modal',
+      SUBMIT_FORM_BUTTON: 'submit-form-button'
   }
 
 $documentBody.click(function(e){
   e.preventDefault();
   var $el = $(e.target),
-    hasDemoRequestClass = $el.hasClass(CLASS_NAMES.REQUEST_BUTTON);
-    isDemoSubmissionModal = $el.parents('.submission-modal').length || $el.hasClass(CLASS_NAMES.SUBMISSION_MODAL),
+    hasDemoRequestClass = $el.hasClass(CLASS_NAMES.BTN_INFO);
+    isDemoSubmissionModal = $el.parents('.demo-submission-modal').length || $el.hasClass(CLASS_NAMES.SUBMISSION_MODAL),
     isSubmitFormButton = $el.hasClass(CLASS_NAMES.SUBMIT_FORM_BUTTON),
     formData = {};
 
   if(hasDemoRequestClass){
     $('.confirmation').addClass('none');
-    $('.form-container').removeClass('none');
+    $('.demo-form-container').removeClass('none');
     $submissionModal.addClass(CLASS_NAMES.OPEN);
   }else if(isSubmitFormButton){
     handleSubmit()
@@ -29,19 +29,19 @@ $documentBody.click(function(e){
 })
 
 function handleSubmit(){
-  var $demoFormValues = $('.form-container input'),
-    $additionalNotesValue = $('.order').val();
+  var $demoFormValues = $('.demo-form-container input'),
+    $additionalNotesValue = $('.notes').val();
 
   $demoFormValues.each(function(item){
     var nameType = $(this).attr('name');
     formData[nameType] = $(this).val();
     $(this).val('');
   })
-  formData['orders'] = $additionalNotesValue;
+  formData['additionalNotes'] = $additionalNotesValue;
   console.log(formData);
   $submissionModal.find('.confirmation').removeClass('none');
   $('notes').val('');
-  $('.form-container').addClass('none');
+  $('.demo-form-container').addClass('none');
 }
 
 });
